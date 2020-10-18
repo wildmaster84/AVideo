d425cfd675b1a8f5f7cbbe504220d8e0057d7bfd
wildmaster84 <46942741+wildmaster84@users.noreply.github.com>
2020-10-18 02:11:58 -0400

    Update indexCol3.php
    Moved Stream Key Field

--- plugin/Live/indexCol3.php
+++ plugin/Live/indexCol3.php
@@ -7,7 +7,6 @@ if (!empty($obj->experimentalWebcam)) {
 
 <div class="tabbable-line"  id="indexTabs">
     <ul class="nav nav-tabs">
-        <li class="active" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Use streaming software or hardware"); ?>"><a data-toggle="tab" href="#tabStreamKey"><i class="fas fa-key"></i> <?php echo __("Stream Key"); ?></a></li>
         <li class="" data-toggle="tooltip" data-placement="bottom" title="<?php echo __("Share information about your live"); ?>"><a data-toggle="tab" href="#tabShare"><i class="fa fa-share"></i> <?php echo __("Share"); ?></a></li>
         <?php
         if (empty($obj->disableRestream)) {
@@ -18,11 +17,6 @@ if (!empty($obj->experimentalWebcam)) {
         ?>
     </ul>
     <div class="tab-content">
-        <div id="tabStreamKey" class="tab-pane fade in active">
-            <?php
-            include $global['systemRootPath'] . 'plugin/Live/tabs/tabStreamKey.php';
-            ?>
-        </div>
         <div id="tabShare" class="tab-pane fade"> 
             <?php
             include $global['systemRootPath'] . 'plugin/Live/tabs/tabShare.php';
